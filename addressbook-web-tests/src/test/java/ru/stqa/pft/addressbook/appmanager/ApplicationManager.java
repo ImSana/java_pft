9fc7492a4755d1e06a751f10d88c3c73b3e84c39
ImSana <aasursyakov@gmail.com>
2019-04-01 01:17:21 +0500

    кое что добавил по браузерам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.appmanager;
+
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   WebDriver wd;
@@ -22,8 +25,15 @@ public class ApplicationManager {
   }
 
   public void init() {
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    String browser = BrowserType.CHROME;
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
+      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
